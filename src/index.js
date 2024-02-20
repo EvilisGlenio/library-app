2fad05bce88fc41286aa09f6da25203849655a3d
Evilis Glenio <glenio.developer@gmail.com>
2024-02-20 12:19:34 -0300

    a fake database created; typeDefs defined; resolvers in development

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
+const { gql, ApolloServer } = require("apollo-server");
 //CREATE FAKE DATABASE
-
+let books = [];
 //DEFINE TYPES
+const typeDefs = gql`
+  type Book {
+    id: ID!
+    title: String
+    author: String
+    publishedAt: Int
+  }
 
+  type Query {
+    books: [Book]
+    book(id: ID!): Book
+  }
+
+  type Mutation {
+    create(id: ID!, title: String!, author: String!, publishedAt: Int!): Book
+    delete(id: ID!): Boolean
+    update(id: ID!, title: String, author: String, publishedAt: Int): Book
+  }
+`;
 //DEFINE RESOLVERS
-
+const resolvers = {
+  Query: {
+    books: () => {
+      return books;
+    },
+    book: (_, { id }) => {
+      return books.find((book) => book.id === id);
+    },
+  },
+};
 //CREATE SERVER
 
 //RUN SERVER
